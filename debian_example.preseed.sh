48fee9cfccaeac89832e175ffdd0da44762b5a3e
Tancredi-Paul Grozav <paul@grozav.info>
2020-02-28 09:33:10 +0000

    Update debian_example.preseed.sh

--- debian_example.preseed.sh
+++ debian_example.preseed.sh
@@ -1,5 +1,14 @@
 # ============================================================================ #
 # Author: Tancredi-Paul Grozav <paul@grozav.info>
 # ============================================================================ #
-echo "I run."
+echo "I run." &&
+
+# Disable FrameBuffer(switching to video mode), for both the kernel and GRUB
+# Useful for when running the machine in QEMU's curses mode.
+sed "/^.*linux.*\/vmlinuz-.* vga=normal /s/ quiet$/ bochs_drm.fbdev=off/" \
+  -i /boot/grub/grub.cfg &&
+sed "/^terminal_output gfxterm/s/ gfxterm$/ console/" -i /boot/grub/grub.cfg &&
+
+echo "I am done running." &&
+exit 0
 # ============================================================================ #
