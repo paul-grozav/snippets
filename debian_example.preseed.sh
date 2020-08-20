d836710d60b7f2bf7e516805a850fc179d8c1632
Tancredi-Paul Grozav <paul@grozav.info>
2020-08-20 10:40:52 +0300

    Update debian_example.preseed.sh

--- debian_example.preseed.sh
+++ debian_example.preseed.sh
@@ -3,14 +3,10 @@
 # ============================================================================ #
 echo "I run." &&
 
-# Disable FrameBuffer(switching to video mode), for both the kernel and GRUB
-# Useful for when running the machine in QEMU's curses mode.
-echo "Disabling video mode - boot in text only ..." &&
-sed "/^.*linux.*\/vmlinuz-.* vga=normal /s/ quiet$/ bochs_drm.fbdev=off/" \
-  -i /boot/grub/grub.cfg &&
-sed "/^terminal_output gfxterm/s/ gfxterm$/ console/" -i /boot/grub/grub.cfg &&
 
 # Remove unused packages
+# ============================================================================ #
+echo &&
 echo "Removing packages to minimal ..." &&
 apt purge -y \
   gnome* *xorg* cpp* desktop-base desktop-file-utils cron geoip-database git \
@@ -20,6 +16,36 @@ apt purge -y \
   libxvmc1 \
   &&
 apt -y autoremove &&
+
+
+# Upgrade system
+# ============================================================================ #
+echo &&
+echo "Upgrading all system packages ..." &&
+apt update &&
+apt upgrade &&
+apt dist-upgrade &&
+apt autoremove &&
+
+
+# Remove unused kernels?
+# ============================================================================ #
+echo &&
+echo "Removing unused kernel packages ..." &&
+apt purge $(dpkg -l | grep linux-image | awk '{print$2}' |
+  grep "\." | grep -v "linux-image-$(uname -r)")
+
+
+
+# Disable FrameBuffer(switching to video mode), for both the kernel and GRUB
+# Useful for when running the machine in QEMU's curses mode.
+# ============================================================================ #
+echo &&
+echo "Disabling video mode - boot in text only ..." &&
+sed "/^.*linux.*\/vmlinuz-.* vga=normal /s/ quiet$/ bochs_drm.fbdev=off/" \
+  -i /boot/grub/grub.cfg &&
+sed "/^terminal_output gfxterm/s/ gfxterm$/ console/" -i /boot/grub/grub.cfg &&
+
 
 echo "I am done running." &&
 exit 0
